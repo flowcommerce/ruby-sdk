8007229658039e310785c83678e8177d69869b27
Michael Bryzek <mbryzek@alum.mit.edu>
2016-06-22 22:41:55 -0400

    Document fuller item creation

--- examples/create_items.rb
+++ examples/create_items.rb
@@ -18,9 +18,29 @@ module CreateItems
                                    :number => number,
                                    :locale => "en_US",
                                    :name => "Flow Test Item #{number}",
+                                   :description => "Only the finest fabric...",
                                    :currency => Util.pick_n(currencies, 1).first,
                                    :price => 100 + rand(120),
                                    :categories => Util.pick_n(categories, 2),
+                                   :attributes => [
+                                     { :key => "size", :value => "small" },
+                                     { :key => "color", :value => "blue" }
+                                   ],
+                                   :dimensions => [
+                                     {
+                                       :type => "product",
+                                       :depth => { :value => "1", :units => "foot" },
+                                       :length => { :value => "18", :units => "inch" },
+                                       :width => { :value => "36", :units => "inches" },
+                                       :weight => { :value => "1.5", :units => "pounds" }
+                                     }
+                                   ],
+                                   :images => [
+                                     {
+                                       :url => "http://lorempixel.com/200/400/fashion/",
+                                       :tags => ["test"]
+                                     }
+                                   ]
                                  )
                                 )
     end
