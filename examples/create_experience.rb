5eb5589d5b5d117af057e8032658eba2f5c387a6
Michael Bryzek <mbryzek@alum.mit.edu>
2016-07-25 11:04:02 -0400

    Upgrade to latest API

--- examples/create_experience.rb
+++ examples/create_experience.rb
@@ -1,22 +1,17 @@
 module CreateExperience
 
   def CreateExperience.run(client, org)
-    country = "CAN"
-
-    subcatalog = client.subcatalogs.post(
-      org,
-      ::Io::Flow::V0::Models::SubcatalogForm.new(:country => country)
-    )
+    region_id = "can"
 
     exp = client.experiences.post(org,
                                   ::Io::Flow::V0::Models::ExperienceForm.new(
-                                    :country => country,
-                                    :name => country,
-                                    :subcatalog_id => subcatalog.id
+                                    :region_id => region_id,
+                                    :name => "canada",
+                                    :language => "en" # Required as canada has en or fr
                                   )
                                  )
 
-    puts "Experience Created: name[%s] key[%s] country[%s]" % [exp.name, exp.key, exp.country]
+    puts "Experience Created: name[%s] key[%s] region[%s]" % [exp.name, exp.key, exp.region.id]
   end
 
 end
