5c253c026a27dc45dc56e7fcd0800bc834f12890
Michael Bryzek <mbryzek@alum.mit.edu>
2016-08-17 11:18:41 -0400

    Handle 422 in example when deleting experiences

--- examples/delete_all_experiences.rb
+++ examples/delete_all_experiences.rb
@@ -3,7 +3,15 @@ module DeleteAllExperiences
   def DeleteAllExperiences.run(client, org)
     client.experiences.get(org).each do |exp|
       puts " - Deleting experience name[%s] key[%s] region[%s]" % [exp.name, exp.key, exp.region.id]
-      client.experiences.delete_by_key(org, exp.key)
+      begin
+        client.experiences.delete_by_key(org, exp.key)
+      rescue Io::Flow::V0::HttpClient::ServerError => e
+        if e.code == 422
+          puts "   - Failed: #{e.message}"
+        else
+          raise e
+        end
+      end
     end
   end
 
