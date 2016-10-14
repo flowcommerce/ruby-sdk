e58d8101c60ff1a63cc430aaeb12dd760ddb7d8b
Michael Bryzek <mbryzek@alum.mit.edu>
2016-10-14 17:12:15 -0400

    Ask for confirmation before deleting experiences

--- examples/delete_all_experiences.rb
+++ examples/delete_all_experiences.rb
@@ -1,6 +1,12 @@
 module DeleteAllExperiences
 
   def DeleteAllExperiences.run(client, org)
+    confirmation = Util::Ask.for_string("Type in name of org to confirm: ")
+    if confirmation != org
+      puts "Invalid entry - existing"
+      exit(1)
+    end
+
     client.experiences.get(org).each do |exp|
       puts " - Deleting experience name[%s] key[%s] region[%s]" % [exp.name, exp.key, exp.region.id]
       begin
