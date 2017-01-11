e78a7ed52acc86032e030f2da4f79c3d02435917
Michael Bryzek <mbryzek@alum.mit.edu>
2017-01-11 10:31:01 -0500

    Add counts

--- examples/set_countries_of_origin.rb
+++ examples/set_countries_of_origin.rb
@@ -3,18 +3,20 @@ module SetCountriesOfOrigin
   def SetCountriesOfOrigin.run(client, org)
     coo = Util::Ask.for_string("Countries of origin (Space separated iso3 codes): ").upcase
 
-    count = 1
     offset = 0
     limit = 100
-    number_updated = 0
+
+    number_changed = 0
+    number_unchanged = 0
+    
     while true
       items = client.items.get(org, :limit => limit, :offset => offset)
 
       items.each do |item|
         existing = item.attributes['countries_of_origin']
         if existing != coo
-          print "#{count}. item[#{item.number}] updating countries of origin from '#{existing}' to '#{coo}'... "
-          count += 1
+          print "#{number_changed+1}. item[#{item.number}] updating countries of origin from '#{existing}' to '#{coo}'... "
+          number_changed += 1
 
           item.attributes[:countries_of_origin] = coo
           map = item.to_hash
@@ -25,6 +27,8 @@ module SetCountriesOfOrigin
           form = ::Io::Flow::V0::Models::ItemForm.new(map)
           client.items.put_by_number(org, item.number, form)
           puts "Done"
+        else
+          number_unchanged += 1
         end
       end
       
@@ -34,6 +38,9 @@ module SetCountriesOfOrigin
         offset += limit
       end      
     end
+
+    puts "# Items Updated: #{number_changed}"
+    puts "# Items Unchanged: #{number_unchanged}"
   end
 
 end
