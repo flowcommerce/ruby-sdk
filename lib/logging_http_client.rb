115ff71c6d5a21ae8a179e4067711ba53968cb3f
Michael Bryzek <mbryzek@alum.mit.edu>
2017-03-19 20:51:10 -0400

    Add comment

--- lib/logging_http_client.rb
+++ lib/logging_http_client.rb
@@ -10,6 +10,7 @@ class LoggingHttpClient < ::Io::Flow::V0::HttpClient::DefaultHttpHandler
     puts "[%s] start %s %s" % [start_time.iso8601(6), request.method, request.path]
 
     if request.path.start_with?("/organizations")
+      # Contrived example to show how client settings can be adjusted
       client.open_timeout = 60
       client.read_timeout = 60
     end
@@ -17,12 +18,12 @@ class LoggingHttpClient < ::Io::Flow::V0::HttpClient::DefaultHttpHandler
     begin
       super
     ensure
+      client.open_timeout = original_open
+      client.read_timeout = original_read
+
       end_time = Time.now.utc.round(10)
       duration = ((end_time - start_time)*1000).round(0)
       puts "[%s] complete %s ms %s %s" % [end_time.iso8601(6), duration, request.method, request.path]
-
-    client.open_timeout = original_open
-      client.read_timeout = original_read
     end    
     
   end
