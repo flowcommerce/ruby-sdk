9ff21eaf6acac2db6ec311ba553b5183ef2e74f7
Michael Bryzek <mike@flow.io>
2019-11-03 12:07:47 -0500

    Fix version extraction

--- scripts/update-apibuilder.rb
+++ scripts/update-apibuilder.rb
@@ -13,7 +13,7 @@ dir = File.dirname(__FILE__)
 
 def extract_version(path)
   IO.readlines(path).each do |l|
-    if md = l.strip.match(/apibuilder.+\/flow\/api\/([\d\.]+)/i)
+    if md = l.strip.match(/apibuilder\s+([\d\.]+)/i)
       return md[1]
     end
   end
